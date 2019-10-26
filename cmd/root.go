9e51d2e74d106b1e0571cf885ed9a58ee14eebd0
Suhail Patel <me@suhailpatel.com>
2019-10-26 17:57:28 +0100

    Start with some boilerplate scaffolding

---
+++ cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "seastat",
+	Short: "Seastat is a Cassandra Prometheus Exporter",
+	Long: `Seastat is a speedy Cassandra Prometheus Exporter 🏎️
+
+You point it at an instance running Cassandra 3.0+ and it'll go and
+poll for metrics periodically in a speedy manner via Jolokia`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// global flags
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "seastat.yaml", "config file (default is seastat.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		log.Info("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/suhailpatel/seastat
+
+go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
+)
