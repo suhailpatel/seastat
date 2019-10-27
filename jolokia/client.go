c4aa618882c58d7c3be32793c96500213e80bff6
Suhail Patel <me@suhailpatel.com>
2019-10-27 10:40:46 +0000

    Add the ability to get GC Stats, Connected Clients and CQL Statement info

--- jolokia/client.go
+++ jolokia/client.go
@@ -35,7 +35,7 @@ func Init(endpoint string) Client {
 func (c *jolokiaClient) Version() (string, error) {
 	v, err := c.get("/jolokia/version")
 	if err != nil {
-		return "", fmt.Errorf("error whilst calling /version: %v", err)
+		return "", fmt.Errorf("err calling /version: %v", err)
 	}
 	return v.Get("value", "agent").String(), nil
 }
@@ -47,11 +47,10 @@ func (c *jolokiaClient) Tables() ([]Table, error) {
 	// a ton of time and CPU usage)
 	v, err := c.read("org.apache.cassandra.metrics", "type=Table", "name=LiveDiskSpaceUsed", "*")
 	if err != nil {
-		return nil, fmt.Errorf("error whilst calling /read for tables: %v", err)
+		return nil, fmt.Errorf("err reading tables: %v", err)
 	}
 
 	tables := []Table{}
-	fmt.Println(v.Get("value").String())
 	v.Get("value").GetObject().Visit(func(key []byte, _ *fastjson.Value) {
 		attributes := extractAttributeMap(string(key))
 
@@ -66,17 +65,80 @@ func (c *jolokiaClient) Tables() ([]Table, error) {
 	return tables, nil
 }
 
-// MemoryStats returns memory information about the Java process
-func (c *jolokiaClient) MemoryStats() (*MemoryStats, error) {
-	v, err := c.read("java.lang", "type=Memory/*")
+// CQLStats returns info about the kinds of CQL statements being processed and
+// how many were prepared vs non-prepared. It also gives some insight into the
+// Prepared Statement cache
+func (c *jolokiaClient) CQLStats() (CQLStats, error) {
+	v, err := c.read("org.apache.cassandra.metrics", "type=CQL", "name=*")
 	if err != nil {
-		return nil, fmt.Errorf("error whilst calling /read for memory stats: %v", err)
+		return CQLStats{}, fmt.Errorf("err reading CQL stats: %v", err)
 	}
 
-	return &MemoryStats{
+	stats := CQLStats{}
+	v.Get("value").GetObject().Visit(func(key []byte, val *fastjson.Value) {
+		attributes := extractAttributeMap(string(key))
+		switch attributes["name"] {
+		case "PreparedStatementsCount":
+			stats.PreparedStatementsCount = val.Get("Count").GetInt64()
+		case "PreparedStatementsEvicted":
+			stats.PreparedStatementsEvicted = val.Get("Count").GetInt64()
+		case "PreparedStatementsExecuted":
+			stats.PreparedStatementsExecuted = val.Get("Count").GetInt64()
+		case "RegularStatementsExecuted":
+			stats.RegularStatementsExecuted = val.Get("Count").GetInt64()
+		case "PreparedStatementsRatio":
+			stats.PreparedStatementsRatio = val.Get("Value").GetFloat64()
+		}
+	})
+	return stats, nil
+}
+
+// ConnectedClients returns the number of connected clients via the Native
+// Protocol in Cassandra
+func (c *jolokiaClient) ConnectedClients() (int64, error) {
+	// We want to be very specific with our query here because otherwise we'll
+	// get a list of all connected clients which might be huge if there are lots
+	// of them!
+	v, err := c.read("org.apache.cassandra.metrics", "type=Client", "name=connectedNativeClients")
+	if err != nil {
+		return 0, fmt.Errorf("err reading clients: %v", err)
+	}
+	return v.Get("value", "Value").GetInt64(), nil
+}
+
+// MemoryStats returns memory information about the Java process
+func (c *jolokiaClient) MemoryStats() (MemoryStats, error) {
+	v, err := c.read("java.lang", "type=Memory/*")
+	if err != nil {
+		return MemoryStats{}, fmt.Errorf("err reading memory stats: %v", err)
+	}
+
+	return MemoryStats{
 		HeapUsed:    v.Get("value", "HeapMemoryUsage", "used").GetInt64(),
 		NonHeapUsed: v.Get("value", "NonHeapMemoryUsage", "used").GetInt64(),
 	}, nil
+}
+
+// GarbageCollectorStatus returns information about Garbage Collections that
+// occur in the process. Since there are different kinds of GC processes
+// occurring, the stats are returned as a list with an item for each kind
+// of GC step
+func (c *jolokiaClient) GarbageCollectionStats() ([]GCStats, error) {
+	v, err := c.read("java.lang", "type=GarbageCollector,*")
+	if err != nil {
+		return []GCStats{}, fmt.Errorf("err reading GC stats: %v", err)
+	}
+
+	stats := []GCStats{}
+	v.Get("value").GetObject().Visit(func(_ []byte, val *fastjson.Value) {
+		stats = append(stats, GCStats{
+			Name:        val.Get("Name").String(),
+			Count:       val.Get("CollectionCount").GetInt64(),
+			LastGC:      time.Duration(val.Get("LastGcInfo", "duration").GetInt64()) * time.Millisecond,
+			Accumulated: time.Duration(val.Get("CollectionTime").GetInt64()) * time.Millisecond,
+		})
+	})
+	return stats, nil
 }
 
 // read is a convinience method around get. It takes in a metric name and a
--- jolokia/jolokia.go
+++ jolokia/jolokia.go
@@ -1,5 +1,7 @@
 package jolokia
 
+import "time"
+
 // Client embeds all the methods which can be called by a Jolokia client
 // running alongside Cassandra
 type Client interface {
@@ -9,15 +11,23 @@ type Client interface {
 	// Tables returns the list of tables from Cassandra
 	Tables() ([]Table, error)
 
-	// MemoryStats returns memory information about the Java process
-	MemoryStats() (*MemoryStats, error)
-}
+	// CQLStats returns info about the kinds of CQL statements being processed
+	// and how many were prepared vs non-prepared. It also gives some insight
+	// into the Prepared Statement cache
+	CQLStats() (CQLStats, error)
 
-// MemoryStats embeds information from the Java memory stats about how much
-// heap and off-heap memory is being utilised
-type MemoryStats struct {
-	HeapUsed    int64
-	NonHeapUsed int64
+	// ConnectedClients returns the number of connected clients via the
+	// Native Protocol in Cassandra
+	ConnectedClients() (int64, error)
+
+	// MemoryStats returns memory information about the Java process
+	MemoryStats() (MemoryStats, error)
+
+	// GarbageCollectorStatus returns information about Garbage Collections
+	// that occur in the process. Since there are different kinds of GC
+	// processes occurring, the stats are returned as a list with an item for
+	// each kind of GC step
+	GarbageCollectionStats() ([]GCStats, error)
 }
 
 // Table embeds information about a Keyspace and Table that exists in
@@ -25,4 +35,29 @@ type MemoryStats struct {
 type Table struct {
 	KeyspaceName string
 	TableName    string
+}
+
+// CQLStats embeds information about Prepared and Regular CQL statements
+// to give insight of the kinds of queries hitting the cluster
+type CQLStats struct {
+	PreparedStatementsCount    int64
+	PreparedStatementsEvicted  int64
+	PreparedStatementsExecuted int64
+	RegularStatementsExecuted  int64
+	PreparedStatementsRatio    float64
+}
+
+// MemoryStats embeds information from the Java memory stats about how much
+// heap and off-heap memory is being utilised
+type MemoryStats struct {
+	HeapUsed    int64 // bytes
+	NonHeapUsed int64 // bytes
+}
+
+// GCStats embeds information for each type of GC that occurs in the process
+type GCStats struct {
+	Name        string
+	Count       int64 // How many collections have occurred
+	LastGC      time.Duration
+	Accumulated time.Duration //
 }
